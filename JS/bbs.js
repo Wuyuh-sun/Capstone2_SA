78da3b7e5b6f2eb064d64d98646a69e9dde7db29
yunha <wjh0970@gmail.com>
2022-04-18 08:41:10 +0900

    map_re_1

--- JS/bbs.js
+++ JS/bbs.js
@@ -1,10 +1,10 @@
 const bbs_listAfter = document.getElementById("bbs_list");
 
 bbs_listAfter.addEventListener("click", function(){
-  if(bbs_listAfter.style.top === "-132px"){
-    bbs_listAfter.style.top = "70px";
-  } else{
+  if(bbs_listAfter.style.top === "70px"){
     bbs_listAfter.style.top = "-132px";
+  } else{
+    bbs_listAfter.style.top = "70px";
   }
   
 })
